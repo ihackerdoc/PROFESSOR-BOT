c2961db815ab631ffaa3ef41386c072a972e13b4
ihackerdoc <ihackerdoc@gmail.com>
2024-08-30 15:10:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,7 +63,9 @@ P_TTI_SHOW_OFF = is_enabled(environ.get('P_TTI_SHOW_OFF', "True"), True)
 PM_IMDB = is_enabled(environ.get('PM_IMDB', "True"), True)
 IMDB = is_enabled(environ.get('IMDB', "True"), True)
 SINGLE_BUTTON = is_enabled(environ.get('SINGLE_BUTTON', "True"), True)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "{file_name}")
+CUSTOM_FILE_CAPTION = environ.get(
+    "CUSTOM_FILE_CAPTION", 
+    "{file_name}\n•────•────────•────•\n📌 ᴊᴏɪɴ : <a href=\"https://t.me/Movie_Search_Group_by_TST\">ᴍᴏᴠɪᴇᴛɪᴍᴇ</a> 🍿\n📌 ᴊᴏɪɴ : <a href=\"https://t.me/tstchitchat\">ᴛꜱᴛ_ᴄʜɪᴛᴄʜᴀᴛ</a>💬\n📌 ꜱᴜʙꜱᴄʀɪʙᴇ ɴᴏᴡ : <a href=\"https://t.me/theshroudtales\">ᴄʟɪᴄᴋ</a> 👈🏻\n•────•────────•────•\n🎗 ʝσιи • ѕнαяє • ѕυρρσят 🎗")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", None)
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", script.IMDB_TEMPLATE)
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
